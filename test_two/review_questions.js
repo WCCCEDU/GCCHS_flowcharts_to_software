01a3fdb8495bda7a1bac0ab7851fc47873b73710
Paul Scarrone <ninjapanzer@gmail.com>
2015-10-26 11:53:45 -0400

    Create review_questions.js

---
+++ test_two/review_questions.js
@@ -0,0 +1,69 @@
+
+
+var number = 5;
+
+// Write an if statement with body that has a "true"
+// condition in terms of the variable "number"
+
+if(number == number){
+	console.log("true");
+}
+
+
+// Write a loop that has 10 iterations and begins
+// at the variable "start"
+
+var start = 10;
+
+// These conditions cover the same range of solutions
+(start >= 1);
+(start > 0);
+
+// As a while loop
+while(start >= 1){
+	start = start - 1;
+}
+
+// As a for loop
+for(start; start > 0; start--){
+
+}
+
+// That pesky identity function
+var identity = function(num){
+	return num;
+}
+
+// Calling and storing the value of a call to the
+// identity function
+var number = identity(1);
+// number -> 1
+
+
+// Scopes
+var start = 1;
+
+while(start<20){
+	start++;
+	var name = "Sara";
+	console.log("hey " + name);
+}
+
+// What is the value of "name" in the global scope
+console.log(name); // what is the output
+
+
+
+var hey = function(){
+	var name2 = "Sarah";
+	console.log("hey " + name2);
+}
+
+// What is the value of "name2" in the global scope
+console.log(name2); // what is the output
+
+hey();
+
+// What is the value of "name2" in the global scope
+console.log(name2); // what is the output
+
