9d7544d00909d1d5a6a2228d8eb6a6376a12a3ac
Paul Scarrone <ninjapanzer@gmail.com>
2015-11-24 10:58:56 -0500

    Create canvas.js

---
+++ canvas/canvas.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("theCanvas");
+var ctx = canvas.getContext("2d");
+ctx.rect(20,20,150,100);
+ctx.stroke();
+
+ctx.fillStyle = "green";
+ctx.fillRect(190, 190, 100, 100);
